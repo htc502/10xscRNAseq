5578d851c9cc7e41dcbece06a9a72e790ebaf5f4
Han <ghan1@Guangchun.local>
2019-10-17 11:25:40 -0500

    rename

--- run.pipeline.sh
+++ run.pipeline.sh
@@ -1,16 +1,23 @@
+##enviroment preparation: install app; module load modules etc
 module load python/3.7.3-anaconda
 module load R/3.6.0
+
+##setup enviroment variables
 ResD='todoFill'
 srcD='todoFill'
 paramD=${srcD}/params
+
+##alias
 runR='Rscript --no-save'
-# ${runR} ${srcD}/load-cellranger.r -c ${paramD}/param-load-cellranger.json -o ${ResD}/loadcellranger.rds
+
+##do the job
+# ${runR} ${srcD}/load-cellranger.r -c ${paramD}/load-cellranger.json -o ${ResD}/loadcellranger.rds
 # ${runR} ${srcD}/qc-doublets-viability.r -d ${ResD}/loadcellranger.rds -o ${ResD}/qc.pdf -c ${paramD}/qc-plotSize.json
-# ${runR} ${srcD}/filter-normalization.r -d ${ResD}/loadcellranger.rds -o ${ResD}/filtered.rds -c ${paramD}/param-filter-normalization.json
-${runR} ${srcD}/exp-export.r -d ${ResD}/filtered.rds -o ${ResD}/userDefinedExp.tsv -c ${paramD}/param-exp-export.json
+# ${runR} ${srcD}/filter-normalize.r -d ${ResD}/loadcellranger.rds -o ${ResD}/filtered.rds -c ${paramD}/filter-normalize.json
+${runR} ${srcD}/exp-export.r -d ${ResD}/filtered.rds -o ${ResD}/userDefinedExp.tsv -c ${paramD}/exp-export.json
 # ${runR} ${srcD}/determinePC.r -d ${ResD}/filtered.rds -o ${ResD}/elBowPlot.pdf
-# ${runR} ${srcD}/snn.r -d ${ResD}/snn.rds -o ${ResD}/snn-markers.rds -c ${paramD}/param-snn.json
-# ${runR} ${srcD}/cluster-markers.r -d ${ResD}/snn.rds -o ${ResD}/snn-markers.tsv -c ${paramD}/param-cluster-markers.json
-# ${runR} ${srcD}/cluster-markers-heatmap.r -d ${ResD}/snn.rds -o ${ResD}/markersHeatmap.pdf -c ${paramD}/param-markers-heatmap.json -m ${ResD}/snn-markers.tsv
-# ${runR} ${srcD}/umap.r -d ${ResD}/snn.rds -o ${ResD}/umap.rds -c ${paramD}/param-umap.json
-# ${runR} ${srcD}/visualize.r -d ${ResD}/umap.rds -o ${ResD}/umap.pdf -c ${paramD}/param-visualize.json
+# ${runR} ${srcD}/snn.r -d ${ResD}/snn.rds -o ${ResD}/snn-markers.rds -c ${paramD}/snn.json
+# ${runR} ${srcD}/snn-marker.r -d ${ResD}/snn.rds -o ${ResD}/snn-markers.tsv -c ${paramD}/snn-marker.json
+# ${runR} ${srcD}/snn-heatmap.r -d ${ResD}/snn.rds -o ${ResD}/markersHeatmap.pdf -c ${paramD}/snn-heatmap.json -m ${ResD}/snn-markers.tsv
+# ${runR} ${srcD}/umap.r -d ${ResD}/snn.rds -o ${ResD}/umap.rds -c ${paramD}/umap.json
+# ${runR} ${srcD}/visualize.r -d ${ResD}/umap.rds -o ${ResD}/umap.pdf -c ${paramD}/visualize.json
